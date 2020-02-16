df94ae0e7f32bad7b3a6a143088975480d9d3970
ajalife <51293183+ajalife@users.noreply.github.com>
2020-02-15 22:46:30 -0300

    Update stack.c

--- Stack/stack.c
+++ Stack/stack.c
@@ -16,12 +16,12 @@ struct stack {
  * *****************************************************************/
 
 bool _stack_resize(stack_t* stack, size_t new_cap){
-	/* Resizes the capacity of the stack
+    /* Resizes the capacity of the stack
        Pre: stack created 
        Post: the stack changed its capacity to "new_cap" and returns true. 
        Or it returns false if it couldn't resize it, and the stack remains intact.*/
 
-	void* new_data = realloc(stack->data, new_cap * sizeof(void*));
+    void* new_data = realloc(stack->data, new_cap * sizeof(void*));
 
     if (!new_data) return false;
   
@@ -31,9 +31,9 @@ bool _stack_resize(stack_t* stack, size_t new_cap){
 }
 
 stack_t* new_stack(){
-	stack_t* stack = malloc(sizeof(stack_t));
+    stack_t* stack = malloc(sizeof(stack_t));
 
-	if (!stack) return NULL;
+    if (!stack) return NULL;
    
     stack->data = malloc(INICIAL_CAPACITY * sizeof(void*));	
     if (!stack->data){
@@ -47,35 +47,35 @@ stack_t* new_stack(){
 }
 
 void delete_stack(stack_t *stack){
-	free(stack->data);
-	free(stack);
+    free(stack->data);
+    free(stack);
 }
 
 bool empty_stack(const stack_t *stack){
-	return (stack->quantity == 0);				
+    return (stack->quantity == 0);				
 }	
 
 bool stack_push(stack_t *stack, void* valor){
-	if (stack->quantity  == stack->capacity){
-		if (!_stack_resize(stack, stack->capacity * RESIZE_INCREASE)) return false;
-	} 
-	stack->data[stack->quantity] = valor;
-	stack->quantity += 1;
-	return true;
+    if (stack->quantity  == stack->capacity){
+    	if (!_stack_resize(stack, stack->capacity * RESIZE_INCREASE)) return false;    
+    } 
+    stack->data[stack->quantity] = valor;
+    stack->quantity += 1;
+    return true;
 }
 	
 void* stack_top(const stack_t *stack){
-	if (empty_stack(stack)) return NULL;
+    if (empty_stack(stack)) return NULL;
 
-	return stack->data[stack->quantity - 1];
+    return stack->data[stack->quantity - 1];
 }
 
 void* stack_pop(stack_t *stack){
-	if (empty_stack(stack)) return NULL;
+    if (empty_stack(stack)) return NULL;
 	
-	if (stack->quantity == stack->capacity / RESIZE_CONDITION){
-		_stack_resize(stack, stack->capacity / RESIZE_DECREASE);
-	}
-	stack->quantity -= 1;
-	return stack->data[stack->quantity];
+    if (stack->quantity == stack->capacity / RESIZE_CONDITION){
+    	_stack_resize(stack, stack->capacity / RESIZE_DECREASE);
+    }
+    stack->quantity -= 1;
+    return stack->data[stack->quantity];
 }
