805d54453b3c97191c68b4e075e5f739c8eb9aa8
ajalife <51293183+ajalife@users.noreply.github.com>
2020-02-15 22:40:48 -0300

    Add files via upload

---
+++ Linked_queue/linked_queue.c
@@ -0,0 +1,92 @@
+#include "linked_queue.h"
+#include <stdlib.h>
+
+// Defino estructuras
+
+typedef struct node{
+	void* data;
+	struct node* next;
+}node_t;
+
+struct queue {
+    node_t* first;
+    node_t* last;
+};
+
+queue_t* new_queue(){
+	queue_t* queue = malloc(sizeof(queue_t));
+
+	if (!queue)	return NULL;
+
+	queue->first = NULL;
+	queue->last = NULL;
+
+	return queue;
+}
+
+node_t* _new_node(void* value){
+	node_t* node = malloc(sizeof(node_t));
+
+	if (!node) return false;
+	
+	node->data = value;
+	node->next = NULL;
+
+	return node;
+}
+
+bool empty_queue(const queue_t *queue){
+	if (!queue)	return false;
+	
+	return (!queue->first);
+}
+
+void* queue_dequeue(queue_t *queue){
+	if (!queue||empty_queue(queue)) return NULL;
+	
+	void* value = queue->first->data;
+	node_t* aux_node = queue->first->next;
+	free(queue->first);
+
+	if (queue->first == queue->last){
+		queue->last = NULL;
+		queue->first = NULL;
+	} else {
+		queue->first = aux_node;
+	}
+	return value;
+}
+
+void delete_queue(queue_t *queue, void destroy_data(void*)){
+	if (!queue) return;
+	
+	node_t* current = queue->first;
+	node_t* next = NULL;
+	while (current){
+		if (destroy_data) destroy_data(current->data);
+
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	free(queue);
+}
+
+bool queue_enqueue(queue_t *queue, void* value){
+	if (!queue) return false;
+	
+	node_t* node = _new_node(value);
+	if (!empty_queue(queue)){
+		queue->last->next = node;
+	} else {
+		queue->first = node;
+	}
+	queue->last = node;
+	return true;
+}
+
+void* queue_first(const queue_t *queue){
+	if (!queue|| empty_queue(queue)) return NULL;
+	
+	return queue->first->data;
+}
---
+++ Linked_queue/linked_queue.h
@@ -0,0 +1,47 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>
+
+
+/* ******************************************************************
+ *               		 DATA TYPE DEFINITIONS
+ * *****************************************************************/
+
+struct queue;
+typedef struct queue queue_t;
+
+
+/* ******************************************************************
+ *                    PRIMITIVAS DE LA queue
+ * *****************************************************************/
+
+// Creates a new queue.
+// Post: returns the empty queue.
+queue_t* new_queue(void);
+
+// Deletes the queue. If destroy_data is received, for every element of the queue, 
+// this function will be aplied to it.
+// Pre: the queue was created, destroy_data is a function capable of destroying 
+// the elementss of the queue.
+// Post: all elements of the queue are deleted.
+void delete_queue(queue_t *queue, void destroy_data(void*));
+
+// Returns true if the queue is empty, false otherwise.
+// Pre: the queue was created.
+bool empty_queue(const queue_t *queue);
+
+// Enqueues a new element, returns flase in case of an error. 
+// Pre: the queue was created.
+bool queue_enqueue(queue_t *queue, void* valor);
+
+// Returns the first element of the queue. If its empty, it returns NULL.
+// Pre: the queue was created.
+void* queue_first(const queue_t *queue);
+
+// It returns the first element of the queue and it deletes it from the queue. If its empty
+// it returns NULL.  queue.
+// Pre: the queue was created.
+void* queue_dequeue(queue_t *queue);
+
+#endif // QUEUE_H
